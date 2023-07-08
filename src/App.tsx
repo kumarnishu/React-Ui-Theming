9ccd23bc36e56e915d324e6c784e82dcca856d69
nishu kumar <kumarnishu437@gmail.com>
2023-07-08 22:47:57 +0530

    theming with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,74 @@
+import  { useMemo, useState } from "react";
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  IconButton,
+  PaletteMode,
+  ThemeOptions,
+  ThemeProvider,
+} from "@mui/material";
+import DarkIcon from "@mui/icons-material/Brightness4";
+import LightIcon from "@mui/icons-material/Brightness7";
+import { blue, orange, pink, red } from "@mui/material/colors";
+
+const darkTheme: ThemeOptions = {
+  palette: {
+    mode: "dark",
+    primary: blue,
+    secondary: pink,
+  },
+};
+
+const lightTheme: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: red,
+    secondary: orange,
+  },
+};
 
 function App() {
+  const [mode, setMode] = useState<PaletteMode>("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === "light" ? "dark" : "light"
+        );
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () => createTheme(mode === "light" ? lightTheme : darkTheme),
+    [mode]
+  );
+
   return (
-    <div className="container py-4 px-3 mx-auto text-sky">
-      <h1>Hello, Bootstrap and Vite!</h1>
-    </div>
-  )
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <Box
+          sx={{
+            display: "flex",
+            minHeight: "100vh",
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {theme.palette.mode} mode
+          <IconButton
+            sx={{ ml: 1 }}
+            onClick={colorMode.toggleColorMode}
+            color="inherit"
+          >
+            {theme.palette.mode === "dark" ? <LightIcon /> : <DarkIcon />}
+          </IconButton>
+        </Box>
+      </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
